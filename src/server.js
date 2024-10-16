f1e2b1c29e5c033aa19bf0f2566b7ca2fadc6c71
Naphtali Morage <naphtalimorage56@example.com>
2024-10-16 11:22:47 +0300

    checking the username or email existence in the database

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const mysql = require("mysql");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-
 const app = express();
 
 app.use(cors());
@@ -18,11 +17,13 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.log("Sorry! invalid connection", err);
+  }
   console.log("Connected to MySQL!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
@@ -30,44 +31,65 @@ app.post("/register", (req, res) => {
       return res.status(400).json({ error: "All fields are required." });
     }
 
-    const query =
-      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-
-    connection.query(query, [username, email, password], (error, results) => {
+    const checkUserQuery =
+      "SELECT * FROM users WHERE username= ? OR email= ? ";
+    //check if the user already exist in the database
+    connection.query(checkUserQuery, [username, email], (error, results) => {
       if (error) throw error;
-      res.json({ message: "User registered successfully" });
+
+      if (results.length > 0) {
+        
+        return res.status(409).json({ error: "User with this email or username already exists" });
+         
+      }
+
+      const insertUSerQuery =
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+
+      connection.query(insertUSerQuery, [username, email, password], (error, results) => {
+          if (error) throw error;
+          res.status(201).json({ message: "User registered successfully" });
+        });
+
     });
   } catch (error) {
     return res.status(500).json({ error: "Registration failed" });
   }
 });
 
+app.post("/login", (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(username, password);
+    if (!username || !password)
+      return res.status(400).json({ error: "All fields are required" });
 
-app.post('/login',  (req, res) => {
-    try{
-        const {username, password} = req.body;
-        console.log(username, password);
-        if(!username || !password) return res.status(400).json({ error: "All fields are required" });
-            
-        const query = "SELECT * FROM users WHERE username =?";
-        connection.query(query, [username], (error, result) => {
-            if(error) throw error;
-            if(result.length === 0){
-                return res.status(401).json({message: "Username or password is incorrect. Please try again."});
-            }
+    const query = "SELECT * FROM users WHERE username =?";
+    connection.query(query, [username], (error, result) => {
+      if (error) throw error;
+      if (result.length === 0) {
+        return res
+          .status(401)
+          .json({
+            message: "Username or password is incorrect. Please try again.",
+          });
+      }
 
-            const user = result[0];
-            if(user.password === password) {
-                res.json({message: "User logged in successfully"});
-            } else {
-                res.status(401).json({message: "username or password is incorrect. Please try again."});
-            }
-        })
-
-    }catch(error){
-        return res.status(500).json({error: "Login failed"});
-    }
-})
+      const user = result[0];
+      if (user.password === password) {
+        res.json({ message: "User logged in successfully" });
+      } else {
+        res
+          .status(401)
+          .json({
+            message: "username or password is incorrect. Please try again.",
+          });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Login failed" });
+  }
+});
 app.listen(5000, () => {
   console.log("Server is running on port 5000...");
 });
